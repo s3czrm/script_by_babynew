c3e4adab206754dfc9f7c53b579f4207318ea46b
爻 <92191927+s3czrm@users.noreply.github.com>
2024-07-16 18:15:49 +0800

    更新 91aw.js

--- m3u8/91aw.js
+++ m3u8/91aw.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 
-https:\/\/\S+\.m3u8\?(token=[^&]+&c=https:\/\/\S+|auth_key=[^&]) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+https:\/\/\S+\.m3u8\?(token=[^&]+&c=https:\/\/\S+|auth_key=[^&]) url script-request-header https://raw.githubusercontent.com/s3czrm/script_by_babynew/main/m3u8/91anwang.js
 
 [mitm]
 hostname = *.cloudfront.net, *.ninghaixin.club,10play.shjtokj.cn
