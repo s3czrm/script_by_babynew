190834bb838155ae066c4ad4c0d4ba1a8530c9ba
爻 <92191927+s3czrm@users.noreply.github.com>
2024-07-16 18:08:34 +0800

    更新 91aw.js

--- m3u8/91aw.js
+++ m3u8/91aw.js
@@ -3,8 +3,8 @@
 
 [rewrite_local]
 
-https:\/\/\S+\.m3u8\?token=[^&]+&c=https:\/\/\S+ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
+https:\/\/\S+\.m3u8\?(token=[^&]+&c=https:\/\/\S+|auth_key=[^&]) url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/m3u8/91anwang.js
 
 [mitm]
-hostname = *.cloudfront.net, *.ninghaixin.club
+hostname = *.cloudfront.net, *.ninghaixin.club,10play.shjtokj.cn
 */
